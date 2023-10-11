965078ed18d605d2627771f02c757bc508ac080c
Edinaaak <edinakucevic@live.com>
2023-10-11 13:19:46 +0200

    add comments

--- klasa.php
+++ klasa.php
@@ -30,10 +30,10 @@ class shopProduct
 
 
 $product1 = new shopProduct("novoIme", "novoPrezime", "novaCena", "noviTitl");
-$product1->name = "Product1";
-$product1->surname = "PrSurname";
-$product1->price = 100;
-$product1->title = "Title1";
+// $product1->name = "Product1";
+// $product1->surname = "PrSurname";
+// $product1->price = 100;
+// $product1->title = "Title1";
 
 echo $product1->name . "<br>";
 echo $product1->surname . "<br>";

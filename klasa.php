d8f37e5c65bec473a3b81111f59a2bcfd040008a
Edinaaak <edinakucevic@live.com>
2023-10-11 13:08:18 +0200

    add object of class

--- klasa.php
+++ klasa.php
@@ -7,4 +7,17 @@ class shopProduct
     public $price;
     public $title;
 }
+
+
+
+$product1 = new shopProduct();
+$product1->name = "Product1";
+$product1->surname = "PrSurname";
+$product1->price = 100;
+$product1->title = "Title1";
+echo $product1->name . "\n";
+echo $product1->surname  . "\n";
+echo $product1->price  . "\n";
+echo $product1->title  . "\n";
 ?>
+

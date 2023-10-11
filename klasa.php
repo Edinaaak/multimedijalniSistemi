83f3a2ad82c0aa2bcb1ea5154551df5ef40f1fec
Edinaaak <edinakucevic@live.com>
2023-10-11 13:15:01 +0200

    add function

--- klasa.php
+++ klasa.php
@@ -6,6 +6,16 @@ class shopProduct
     public $surname;
     public $price;
     public $title;
+
+    public function set_name($name)
+    {
+        $this->name = $name;
+    }
+
+    public function get_name_surname()
+    {
+        return $this->name . " " . $this->surname;
+    }
 }
 
 
@@ -15,9 +25,14 @@ $product1->name = "Product1";
 $product1->surname = "PrSurname";
 $product1->price = 100;
 $product1->title = "Title1";
-echo $product1->name . "\n";
-echo $product1->surname  . "\n";
-echo $product1->price  . "\n";
-echo $product1->title  . "\n";
+
+echo $product1->name . "<br>";
+echo $product1->surname . "<br>";
+echo $product1->price . "<br>";
+echo $product1->title . "<br>";
+
+echo $product1->set_name("new_name");
+echo $product1->get_name_surname();
+
 ?>
 

1b0b6437482b31c97ca22c46fd048f5ae713218c
Edinaaak <edinakucevic@live.com>
2023-10-16 08:27:14 +0200

    definisalaResolve

--- address.php
+++ address.php
@@ -3,13 +3,21 @@ class AddressManager
 {
     private $addresses = ["209.131.36.159", "216.58.213.174"];
 
-    public function outputAddress($resolve)
+    public function outputAddress(bool $resolve)
     {
         foreach ($this->addresses as $address) {
             echo $address;
+            
             if ($resolve) {
-                echo " (" . gethostbyaddr($address) . ")";
+                $resolvedAddress = gethostbyaddr($address);
+                
+                if ($resolvedAddress !== $address) {
+                    echo " (Resolved: " . $resolvedAddress . ")";
+                } else {
+                    echo " (Not Resolved)";
+                }
             }
+            
             echo "<br>";
         }
     }
@@ -17,10 +25,10 @@ class AddressManager
 
 // XML postavke za resolvedomains
 $xml = simplexml_load_string('<settings><resolvedomains>false</resolvedomains></settings>');
-$resolve = $xml->resolvedomains == 'true';
-
+$resolve = ($xml->resolvedomains == 'true') ? true : false;
 
 $manager = new AddressManager();
 $manager->outputAddress($resolve);
+
 
 ?>

1f7ea16b9c9510c033dc5068fb0989ae45c7cf26
Edinaaak <edinakucevic@live.com>
2023-10-16 09:06:37 +0200

    dodalaNovuKlasuStorage

--- address.php
+++ address.php
@@ -1,4 +1,6 @@
 <?php
+
+//mozemo da definisemo striktne promenljive
 class AddressManager
 {
     private $addresses = ["209.131.36.159", "216.58.213.174"];
@@ -30,5 +32,45 @@ $resolve = ($xml->resolvedomains == 'true') ? true : false;
 $manager = new AddressManager();
 $manager->outputAddress($resolve);
 
+
+class Storage {
+    public function addd(string $key, $value)
+    {
+
+    }
+
+    public function add1(string $value, mixed $values2)
+    {
+
+    }
+
+    public function  add3(string $value, bool|string $values)
+    {
+
+
+    }
+
+    public function add4(string $key, ?string $value4)
+    {
+
+    }
+
+    public function getValue():int 
+    {
+        return $this->Value;
+    }
+
+    public function Discount():int|float 
+    {
+        return $this->price-$this->discount;
+    }
+
+
+    public function setDiscount(int|float $num):void
+    {
+        $this->discount = $num;
+    }
+    //nullable tipovi sa prefiksom ?
+}
 
 ?>

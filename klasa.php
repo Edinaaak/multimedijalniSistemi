9280da76015a377c25ae2fc981bfe13d675c501f
Edinaaak <edinakucevic@live.com>
2023-10-16 08:34:22 +0200

    dodalaPrinterKlasu

--- klasa.php
+++ klasa.php
@@ -27,21 +27,30 @@ class shopProduct
     }
 }
 
+class shopPrinter
+{
+
+    function print($name, $surname, $price, $title)
+    {
+        echo "Name: " .$name . " Surname: " .$surname . " Price: " .$price ." Title: " .$title;
+    }
+}
+
 
 
 $product1 = new shopProduct("novoIme", "novoPrezime", "novaCena", "noviTitl");
+$shopPrint = new shopPrinter();
+$shopPrint->print("Name1", "Surname1", "Price1", "Title1");
 // $product1->name = "Product1";
 // $product1->surname = "PrSurname";
 // $product1->price = 100;
 // $product1->title = "Title1";
 
-echo $product1->name . "<br>";
-echo $product1->surname . "<br>";
-echo $product1->price . "<br>";
-echo $product1->title . "<br>";
+// echo $product1->name . "<br>";
+// echo $product1->surname . "<br>";
+// echo $product1->price . "<br>";
+// echo $product1->title . "<br>";
 
-echo $product1->set_name("new_name");
-echo $product1->get_name_surname();
 
 ?>
 
